a2276680ee5e8d625e03b0b662b244f87abf7e33
Péricles Júnior <periclesmjr@hotmail.com>
2024-02-22 19:51:15 -0300

    revisando usos do array

--- 3. Trabalhando com Arrays/array.js
+++ 3. Trabalhando com Arrays/array.js
@@ -10,58 +10,58 @@ const arr = [
 ];
 
 console.log(arr);
-// Adicionar Elementos
-// push
+// push: Adiciona um elemento no final do array e devolve o novo tamanho do array
 let tamanho = arr.push("Boromir");
 console.log(arr);
 console.log(tamanho);
 
-// unshift
+// unshift: Adiciona um elemento no começo do array e devolve o novo tamanho do array
 tamanho = arr.unshift("Teste");
 console.log(arr);
 console.log(tamanho);
 
-// Remover elementos
-// pop
+// pop: Remove um elemento no final do array e devolve o elemento removido
 let ultimoElemento = arr.pop();
 console.log(arr);
 console.log(ultimoElemento);
 
-// shift
+// shift: Remove um elemento no começo do array e devolve o elemento removido
 ultimoElemento = arr.shift();
 console.log(arr);
 console.log(ultimoElemento);
 
-// Pesquisar por um Elemento
-// includes
+// includes: Verifica se um certo elemento está presente no array
 const inclui = arr.includes("Gandalf, o Cinzento");
 console.log(inclui);
 
-// Pesquisar por um índice
-// indexOf
+// indexOf: Encontra o índice que possui o elemento indicado, ou -1 caso ele não exista no array
 const indice = arr.indexOf("Gandalf");
 console.log(indice);
 
-// Cortar e Concatenar
-// slice
+// slice: Copia uma parte do array e devolve a parte copiada sem alterar o array original
 const hobbits = arr.slice(0, 4);
 const outros = arr.slice(-4);
 console.log(arr);
 console.log(hobbits);
 console.log(outros);
 
-// concat
-const sociedade = hobbits.concat(outros, "Boromir")
-console.log(sociedade)
+// concat: Junta dois ou mais arrays e/ou mais itens e devolve o resultado (sem alterar nenhum dos arrays)
+const sociedade = hobbits.concat(outros, "Boromir");
+console.log(sociedade);
 
-// Substituição ou remoção dos elementos
-// splice
-const elementosRemovidos = sociedade.splice(indice, 1, "Gandalf, o Cinzento")
-console.log(sociedade)
-console.log(elementosRemovidos)
+// splice: Permite remover elementos em qualquer posição do array e substituir por novos
+const elementosRemovidos = sociedade.splice(
+  indice,
+  1,
+  "Gandalf, o Cinzento"
+);
+console.log(sociedade);
+console.log(elementosRemovidos);
 
-// Iterar sobre os Elementos
-for (let indice = 0; indice < sociedade.length; indice++){
-  const elemento = sociedade[indice]
-  console.log(elemento + " se encontra na posição " + indice)
+// Iterar sobre os Elementos: Usando o for para percorrer cada elemento do array
+for (let indice = 0; indice < sociedade.length; indice++) {
+  const elemento = sociedade[indice];
+  console.log(
+    elemento + " se encontra na posição " + indice
+  );
 }
