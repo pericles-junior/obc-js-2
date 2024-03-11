eb499f597705e857c62c86425b64417ef588d3f7
Perinho <periclesmjr@hotmail.com>
2024-03-11 13:47:48 -0300

    continuando os estudos com função recursiva

--- 19. Funções recursivas/recursao.js
+++ 19. Funções recursivas/recursao.js
@@ -1,10 +1,34 @@
 function dividir(num){
     console.log(num)
-    if (num % 2 === 0){
+    if (num % 2 === 0){ // condicional para parar a execução
         dividir(num / 2)
     } else {
         return num
     }
 }
 
-dividir(40)
+// dividir(40)
+
+function dobrar(num){
+    console.log(num)
+    dobrar(num * 2)
+}
+
+// Excede o número de chamados da pilha
+// dobrar(1)
+
+
+// fatorial de 5: !5 === 5 * 4 * 3 * 2 * 1 === 5 * 4!
+function fatorial (num){
+    console.log("Número: " + num)
+    if(num === 0){ // caso base
+        return 1
+    } else if(num === 1){
+        return 1
+    } else {
+        console.log(num + " * !" + (num - 1))
+        return num * fatorial(num - 1)
+    }
+} 
+
+console.log(fatorial(5))
